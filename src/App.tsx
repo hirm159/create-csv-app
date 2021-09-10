01e93962bc7e7d9263e7bd2797c6cbf4d3a3a61d
hirm159 <hiro159m@gmail.com>
2021-09-11 01:24:44 +0900

    仮

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import { TopPage } from "./pages/Top/TopPage";
 import { LinkPage } from './pages/Link/LinkPage';
+import { Navbar } from 'react-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Top</Link>
+            </li>
+            <li>
+              <Link to='/link'>Link</Link>
+            </li>
+          </ul>
+        </nav>
       <Switch>
         <Route path="/" exact component={TopPage} />
         <Route path="/link" exact component={LinkPage} />
       </Switch>
+      </div>
     </BrowserRouter>
   );
 }
