30086a31bf392f6183f5ef17d4a9d85a35138652
hirm159 <hiro159m@gmail.com>
2021-09-11 00:00:57 +0900

    画面遷移等追加

---
+++ src/pages/Link/LinkPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+export const LinkPage = () => {
+    return (
+        <div className="App">
+            <h1>リンク集</h1>
+
+        </div>
+    )
+}
---
+++ src/pages/Top/TopPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const TopPage = () => {
+    return (
+        <div className="App">
+            <h1>CSV作成簡易アプリケーション</h1>
+        </div>
+    )
+}
