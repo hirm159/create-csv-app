eea6e4738612d2049b14d1baee31f5a1d10972aa
hirm159 <hiro159m@gmail.com>
2021-10-18 20:59:07 +0900

    headerの追加、apphistory追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import { Router, Switch, Route, Redirect, Link } from 'react-router-dom';
 import TopPage  from "./pages/Top/TopPage";
 import LinkPage from  "./pages/Link/LinkPage";
 import { Navbar, Container, Nav } from 'react-bootstrap';
+import { createBrowserHistory } from 'history';
+
+export const appHistory = createBrowserHistory({ basename: "/create-csv-app" });
 
 class App extends Component {
   render () {
     return (
-      <BrowserRouter>
-        <div>
-          <Navbar bg="light" expand="lg">
-            <Container>
-              <Navbar.Brand href="/">Create-Csv-App</Navbar.Brand>
-              <Navbar.Text>version 1.0.0</Navbar.Text>
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                    <Nav.Link href="/">Top</Nav.Link>
-                    <Nav.Link href="link">Link</Nav.Link>
-                </Nav>
-              </Navbar.Collapse>
-            </Container>
-          </Navbar>
-          <Switch>
-            <Route path="/" exact component={TopPage} />
-            <Route path="/link" exact component={LinkPage} />
-          </Switch>
-        </div>
-      </BrowserRouter>
+      <div>
+        <Router history={appHistory}>
+            <Switch>
+              <Route path="/" exact>
+                <Redirect to="/top" />
+              </Route>
+              <Route path="/top" exact component={TopPage} />
+              <Route path="/link" exact component={LinkPage} />
+            </Switch>
+        </Router>
+      </div>
     );
   }
 }
--- src/pages/Top/TopPage.tsx
+++ src/pages/Top/TopPage.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container, Col } from 'react-bootstrap';
 import InputForm from '../../components/InputForm/InputForm';
 import InputList from '../../components/InputList/InputList';
+import { Header } from '../../components/index';
 
 const titleStyle: React.CSSProperties = {
     marginBottom: "30px"
@@ -11,6 +12,7 @@ export default class TopPage extends React.Component {
     render() {
         return (
             <Container>
+                <Header />
                 <div>
                     <Col md={12}>
                         <h1 style={titleStyle}>CSV作成アプリケーション</h1>
